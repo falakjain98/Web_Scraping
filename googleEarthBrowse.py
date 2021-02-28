a7773f10bea4b63db0c388a2793d474f6dc49905
falakjain <jain.falak@hotmail.com>
2021-02-28 19:42:36 +0530

    Create googleEarthBrowse.py
    launching Google Earth using explicit wait functionality in Selenium

---
+++ googleEarthBrowse.py
@@ -0,0 +1,15 @@
+#! python3
+# googleEarthBrowse.py - launching Google Earth using explicit wait functionality in Selenium
+# Explicit Wait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+url = 'https://www.google.com/earth/'
+driver = webdriver.Chrome()
+driver.get(url)
+
+wait = WebDriverWait(driver,10)
+launchEarthButton = wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/header/div/nav[1]/ul[2]/li[2]/a')))
+launchEarthButton.click()
